59350ce510ea1a12f580391598ec66daf6b9aae6
Michael <michael@mealsloth.com>
2016-07-22 11:40:56 -0700

    minor format change

--- static/js/freya/freya.helper.js
+++ static/js/freya/freya.helper.js
@@ -4,29 +4,29 @@
 
 "use strict";
 
-Freya.Helper = {};
-
-Freya.Helper.CookieHelper = {
-    GetCookie: function(name) {
-        var cookieValue = null;
-        if (document.cookie && document.cookie != "") {
-            var cookies = document.cookie.split(";");
-            for (var i = 0; i < cookies.length; i++) {
-                var cookie = $.trim(cookies[i]);
-                if (cookie.substring(0, name.length + 1) == (name + "=")) {
-                    cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
-                    break;
+Freya.Helper = {
+    CookieHelper: {
+        GetCookie: function (name) {
+            var cookieValue = null;
+            if (document.cookie && document.cookie != "") {
+                var cookies = document.cookie.split(";");
+                for (var i = 0; i < cookies.length; i++) {
+                    var cookie = $.trim(cookies[i]);
+                    if (cookie.substring(0, name.length + 1) == (name + "=")) {
+                        cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+                        break;
+                    }
                 }
             }
+            return cookieValue;
+        },
+        AjaxAppendCSRFHeader: function () {
+            var csrftoken = Freya.Helper.CookieHelper.GetCookie("csrftoken");
+            $.ajaxSetup({
+                beforeSend: function (xhr) {
+                    xhr.setRequestHeader("X-CSRFToken", csrftoken);
+                }
+            });
         }
-        return cookieValue;
-    },
-    AjaxAppendCSRFHeader: function() {
-        var csrftoken = Freya.Helper.CookieHelper.GetCookie("csrftoken");
-        $.ajaxSetup({
-            beforeSend: function (xhr) {
-                xhr.setRequestHeader("X-CSRFToken", csrftoken);
-            }
-        });
     }
 };
